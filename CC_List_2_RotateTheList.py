da755ec86c3b38373fccb47ff5444e5e283535b2
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2022-09-12 20:41:52 +0530

    Update CC_List_2_RotateTheList.py

--- CC_List_2_RotateTheList.py
+++ CC_List_2_RotateTheList.py
@@ -1,1 +1,14 @@
-python
+Inputlist=list(map(int,input("Enter the Numbers in List\n").split(' ')))
+print("Before Rotating :")
+for i in Inputlist:
+    print(i,end=" ")
+print("After Rotating : ")
+
+count=0
+for j in Inputlist:
+    if count!=0:
+        print(j,end=" ")
+    count=count+1
+print(Inputlist[0])
+
+#******************************************************
