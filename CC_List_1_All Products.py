ed5c23caacd5a6f7ea22ac2d195e0dbde59951b3
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2022-09-12 20:51:41 +0530

    Update CC_List_1_All Products.py

--- CC_List_1_All Products.py
+++ CC_List_1_All Products.py
@@ -1,1 +1,12 @@
-python
+CargoIdsList=list(map(int,input("Enter the Numbers in List-1\n").split(" ")))
+CargoPriceList=list(map(int,input("Enter the Numbers in List-2\n").split(" ")))
+
+count=0
+for i in CargoIdsList:
+    for j in CargoPriceList:
+        if((i*j)%2!=0):
+            print(i*j,end=' ')
+            count=1
+            
+if(count==0):
+    print("No such Elements in the list")
